5cf2f1d26b262f8b8244cfffda3d113c0b7f14ae
jmfigueroaGit <jheremiah.figueroa@tup.edu.ph>
2021-12-27 22:47:39 +0800

    Backend Updates

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,118 +1,88 @@
 import room from '../models/room';
 import Room from '../models/room';
+import ErrorHandler from '../utils/errorHandler';
+import catchAsyncError from '../middlewares/catchAsyncError';
+import APIFeatures from '../utils/apiFeatures';
 
-const allRooms = async (req, res) => {
-	try {
-		const rooms = await Room.find();
+const allRooms = catchAsyncError(async (req, res) => {
+	const resPerPage = 2;
+	const roomsCount = await Room.countDocuments();
 
-		res.status(200).json({
-			success: true,
-			count: rooms.length,
-			rooms,
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			error: error.message,
-		});
-	}
-};
+	const apiFeature = new APIFeatures(Room.find(), req.query)
+		.search()
+		.filter()
+		.pagination(resPerPage);
+
+	let rooms = await apiFeature.query;
+	let filteredRoomsCount = rooms.length;
+
+	res.status(200).json({
+		success: true,
+		roomsCount,
+		resPerPage,
+		filteredRoomsCount,
+		rooms,
+	});
+});
 
 // Create new room => /api/rooms
-const newRoom = async (req, res) => {
-	try {
-		const room = await Room.create(req.body);
+const newRoom = catchAsyncError(async (req, res) => {
+	const room = await Room.create(req.body);
 
-		res.status(201).json({
-			success: true,
-			room,
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			error: error.message,
-		});
-	}
-};
+	res.status(201).json({
+		success: true,
+		room,
+	});
+});
 
 // Get Room details => /api/rooms/:id
-const getSingleRoom = async (req, res) => {
-	try {
-		const room = await Room.findById(req.query.id);
+const getSingleRoom = catchAsyncError(async (req, res, next) => {
+	const room = await Room.findById(req.query.id);
 
-		if (!room) {
-			res.status(404).json({
-				success: false,
-				error: 'Room not found with this ID',
-			});
-		}
-
-		res.status(201).json({
-			success: true,
-			room,
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			error: error.message,
-		});
+	if (!room) {
+		return next(new ErrorHandler('Room not found with this ID', 404));
 	}
-};
+
+	res.status(201).json({
+		success: true,
+		room,
+	});
+});
 
 // Update Room details => /api/rooms/:id
-const updateRoom = async (req, res) => {
-	try {
-		let room = await Room.findById(req.query.id);
+const updateRoom = catchAsyncError(async (req, res) => {
+	let room = await Room.findById(req.query.id);
 
-		if (!room) {
-			res.status(404).json({
-				success: false,
-				error: 'Room not found with this ID',
-			});
-		}
-
-		room = await Room.findByIdAndUpdate(req.query.id, req.body, {
-			new: true,
-			runValidators: true,
-			useFindAndModify: false,
-		});
-
-		res.status(201).json({
-			success: true,
-			room,
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			error: error.message,
-		});
+	if (!room) {
+		return next(new ErrorHandler('Room not found with this ID', 404));
 	}
-};
+
+	room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+		new: true,
+		runValidators: true,
+		useFindAndModify: false,
+	});
+
+	res.status(201).json({
+		success: true,
+		room,
+	});
+});
 
 // Delete Room details => /api/rooms/:id
-const deleteRoom = async (req, res) => {
-	try {
-		const room = await Room.findById(req.query.id);
+const deleteRoom = catchAsyncError(async (req, res) => {
+	const room = await Room.findById(req.query.id);
 
-		if (!room) {
-			res.status(404).json({
-				success: false,
-				error: 'Room not found with this ID',
-			});
-		}
-
-		await room.remove();
-
-		res.status(201).json({
-			success: true,
-			message: 'Room is deleted',
-		});
-	} catch (error) {
-		res.status(400).json({
-			success: false,
-			error: error.message,
-		});
+	if (!room) {
+		return next(new ErrorHandler('Room not found with this ID', 404));
 	}
-};
+
+	await room.remove();
+
+	res.status(201).json({
+		success: true,
+		message: 'Room is deleted',
+	});
+});
 
 export { allRooms, newRoom, getSingleRoom, updateRoom, deleteRoom };
